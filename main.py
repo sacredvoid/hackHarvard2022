f07cc044427dd493598bdc666656b874a7a3a578
Samanvya Tripathi <samanu09@gmail.com>
2022-10-15 01:57:21 -0400

    Fix image upload endpoint

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from os import write
+import os
 from fastapi import FastAPI, HTTPException, File, UploadFile
 from pydantic import BaseModel
 import sys
@@ -15,10 +17,21 @@ def read_root():
 
 @app.post("/upload/", response_model=UploadConfirmation)
 async def upload(file: UploadFile = File(...)):
+    if not os.path.isdir(IMAGE_DOWNLOAD_PATH):
+        os.mkdir(IMAGE_DOWNLOAD_PATH)
     try:
         contents = await file.read()
+        input_img_path = IMAGE_DOWNLOAD_PATH+os.sep+file.filename
+        with open(input_img_path,'wb') as f:
+            f.write(contents)
     except Exception:
         e = sys.exc_info()[1]
         raise HTTPException(status_code=500, detail=str(e))
 
+    # IMG2TXT
+    # Tokenizer
+    # TXT2SOUND
+    # SOUNDSYNTH
+    # COMBINE IMAGE+SOUND
+    # RETURN VIDEO REQUEST
     return {"filename": file.filename, "contenttype":file.content_type}
--- post_image_examplereq.py
+++ post_image_examplereq.py
@@ -3,10 +3,10 @@ url = 'http://127.0.0.1:8000/upload/'
 files = {'image': open(r'C:\Users\saman\Downloads\WhatsApp Image 2022-10-14 at 9.13.31 PM.jpeg', 'rb')}
 resp = requests.post(url, files=files)
 
-name_img = 'try'
+name_img = 'try.jpeg'
 
 with open(r'C:\Users\saman\Downloads\WhatsApp Image 2022-10-14 at 9.13.31 PM.jpeg', 'rb') as img:
-    files = {'image': (name_img,img,'multipart/form-data',{'Expires': '0'}) }
+    files = {'file': (name_img,img,'multipart/form-data',{'Expires': '0'}) }
     with requests.Session() as s:
         r = s.post(url,files=files)
         print(r.status_code)
