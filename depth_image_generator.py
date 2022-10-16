f16fffcf9cc5003c2c4d2229e95c1786111e7e00
Rajat Keshri <32040893+rajatkeshri@users.noreply.github.com>
2022-10-16 04:32:20 -0400

    Update depth_image_generator.py

--- depth_image_generator.py
+++ depth_image_generator.py
@@ -3,12 +3,17 @@ import glob
 import numpy as np
 from PIL import Image
 
-from data import TEMP_FILES_PATH, DEPTH_IMAGE_FILE
+from data import TEMP_FILES_PATH, DEPTH_IMAGE_FILE, IMAGE_DOWNLOAD_PATH
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 from keras.models import load_model
 from layers import BilinearUpSampling2D
 from matplotlib import pyplot as plt
+
+from skimage.transform import resize
+from skimage.io import imsave, imread
+
+import cv2
 
 
 def to_multichannel(i):
@@ -81,10 +86,17 @@ def predict(model, images, minDepth=10, maxDepth=1000, batch_size=2):
     return np.clip(DepthNorm(predictions, maxDepth=maxDepth), minDepth, maxDepth) / maxDepth
 
 
+def resizeimage(path):
+    img = imread(path)
+    resized_img = cv2.resize(img, (640, 480))
+    resized_img = resize(img, (640, 480), preserve_range=True, mode='reflect', anti_aliasing=True)
+    imsave(TEMP_FILES_PATH + "test.png", resized_img)
+
 def generate_depth_image(path):
     # Argument Parser
     model = 'depthmodel.h5'
-    inputimage = path
+    resizeimage(path)
+    inputimage = TEMP_FILES_PATH + "test.png"
 
     custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
     model = load_model(model, custom_objects=custom_objects, compile=False)
@@ -93,5 +105,6 @@ def generate_depth_image(path):
     outputs = predict(model, inputs)
 
     viz = display_images(outputs.copy(), inputs.copy())
+    plt.savefig(IMAGE_DOWNLOAD_PATH + "inputdepthimage.jpg")
     plt.figure(figsize=(10, 5))
-    plt.savefig(TEMP_FILES_PATH+DEPTH_IMAGE_FILE)
+
